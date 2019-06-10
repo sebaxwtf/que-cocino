a73c5101e18c7583ec3ae05f5361508adc7f1584
roberto herrera <robertherrerajorquera@gmail.com>
2019-06-10 17:54:00 -0400

    agrega busqueda y id
    se termino la busqueda (front-end) y luego de esto se agrego un js con sus id de cada cosa y luego de se mandaron al php, falta provarlo y ver si muestra el php en pantalla la id del producto seleccionado, y en el caso de que no arreglarlo con metodo que junte todo...

---
+++ consulta.php
@@ -0,0 +1,36 @@
+<?php
+$aa = $_POST["a"];
+echo $aa; 
+// el valor
+$ab = $_POST["b"];
+echo $ab; 
+$ac = $_POST["c"];
+echo $ac; 
+$ad = $_POST["d"];
+echo $ad; 
+$ae = $_POST["e"];
+echo $ae; 
+$af = $_POST["f"];
+echo $af; 
+$ag = $_POST["g"];
+echo $ag; 
+$ah = $_POST["h"];
+echo $ah; 
+$ai = $_POST["i"];
+echo $ai; 
+$aj = $_POST["j"];
+echo $aj; 
+$ak = $_POST["k"];
+echo $ak; 
+$al = $_POST["l"];
+echo $al; 
+$am = $_POST["m"];
+echo $am; 
+$an = $_POST["n"];
+echo $an; 
+$ao = $_POST["o"];
+echo $ao; 
+$ap = $_POST["p"];
+echo $ap; 
+
+?>
---
+++ js/consulta.js
@@ -0,0 +1,36 @@
+function consulta(){
+    var a = document.getElementById("1");
+    var aa = 1;
+    var b = document.getElementById("2");
+    var ab = 2;
+    var c = document.getElementById("3");
+    var ac = 3;
+    var d = document.getElementById("4");
+    var ad = 4;
+    var e = document.getElementById("5");
+    var ae = 5;
+    var f = document.getElementById("6");
+    var af = 6;
+    var g = document.getElementById("7");
+    var ag = 7;
+    var h = document.getElementById("8");
+    var ah = 8;
+    var i = document.getElementById("9");
+    var ai = 9;
+    var j = document.getElementById("10");
+    var aj = 10;
+    var k = document.getElementById("11");
+    var ak = 11;
+    var l = document.getElementById("12");
+    var al = 12;
+    var m = document.getElementById("13");
+    var am = 13;
+    var n = document.getElementById("14");
+    var an = 14;
+    var o = document.getElementById("15");
+    var ao = 15;
+    var p = document.getElementById("16");
+    var ap = 16;
+
+
+}
